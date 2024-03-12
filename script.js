3f33e201798db01479f7a0f74a2c90bd49c2331c
RFozzy688 <pokerfozzy@gmail.com>
2024-03-12 07:16:18 +0200

    страница Cards

--- script.js
+++ script.js
@@ -603,6 +603,396 @@ function visitorsSearch(searchText){
   }
 }
 
+//=================================== PAGE CARDS ==========================================
+
+document.getElementById('pageCards').addEventListener('click', function (event) {
+  container.removeChild(content);
+
+  showPageCards();
+});
+
+function showPageCards(){
+  let newItem = document.createElement('div');
+
+  newItem.id = 'content';
+  newItem.classList.add('content');
+  newItem.innerHTML = createHTMLCards();
+
+  container.appendChild(newItem);
+
+  document.getElementById('newCard').addEventListener('click', function (event) { 
+    let modal = document.getElementById('modal');
+    modal.innerHTML = createModalDialogCard();
+    modal.style.display = 'block';
+
+    initializeComboBoxes();
+  });
+
+  document.getElementById('sort').addEventListener('click', function (event) {
+    let comboBox = document.getElementById('combo');
+    let strSortBy = comboBox.options[comboBox.selectedIndex].text;
+
+    cardsSortBy(strSortBy);
+  });
+
+  document.getElementById('search').addEventListener('click', function (event) {
+    let searchText = document.getElementById('searchText');
+
+    cardsSearch(searchText.value);
+
+    searchText.value = '';
+  });
+
+  let arrayObj = localStorage.getItem('cards');
+  let cards = [];
+
+  if (arrayObj != null){
+    cards = JSON.parse(arrayObj);
+
+    cards.forEach(card => {
+      addCardToTable(card.id, card.visitor, card.book, card.borrowDate, card.returnDate);
+    });
+  }
+
+}
+
+function createHTMLCards(){
+  let pageHTML = `
+  <div class="header">
+    <h3 class="color-title">ALL CARDS:</h3>
+    <button id="newCard" class="btn-new-book">New card</button>
+  </div>
+  <hr>
+  <div class="sort-search">
+    <div class="sort">
+      <label for="combo">Sort by:</label>
+      <select name="list" id="combo">
+        <option value="0">ID</option>
+        <option value="1">Visitor</option>
+        <option value="2">Book</option>
+        <option value="3">Borrow Date</option>
+        <option value="4">Return Date</option>
+      </select>
+      <button id="sort">Sort</button>
+    </div>
+    <div class="search">
+      <label for="searchText">Search:</label>
+      <input id="searchText" type="text">
+      <button id="search">Search</button>
+    </div>
+  </div>
+  <div class="list">
+    <table id="listCards">
+      <tr>
+        <th>ID</th>
+        <th>Visitor</th>
+        <th>Book</th>
+        <th>Borrow Date</th>
+        <th>Return Date</th>
+      </tr>          
+    </table>
+  </div>`
+
+  return pageHTML;
+}
+
+function createModalDialogCard(){
+  return `
+  <div id="modalCard" class="modal-dialog">
+    <div id="modalClose" class="modal-close"><i onclick="closeModalDialog('modalCard')" class="fa fa-times btn" aria-hidden="true"></i></div>
+    <p id="modalTitle" class="modal-title">New card:</p>
+    <label class="label-text" for="comboVisitors">Visitor:</label>
+    <select name="list" class="combo-box" id="comboVisitors"></select>
+    <label class="label-text" for="comboBooks">Book:</label>
+    <select name="list" class="combo-box" id="comboBooks"></select>
+    <button id="createCard" class="modal-btn" onclick="createCard()">Create card</button>
+    <p id="warning" class="warning">Заполните все поля формы</p>
+  </div>`;
+}
+
+function initializeComboBoxes(){
+  let visitors = JSON.parse(localStorage.getItem('visitors'));
+  let books = JSON.parse(localStorage.getItem('books'));
+  let comboVisitors = document.getElementById('comboVisitors');
+  let comboBooks = document.getElementById('comboBooks');
+
+  visitors.forEach(visitor => {
+    let newOption = document.createElement('option');
+    newOption.text = visitor.name;
+    newOption.value = visitor.id;
+    comboVisitors.add(newOption);
+  });
+
+  books.forEach(book => {
+    if (book.quantity > 0){
+      let newOption = document.createElement('option');
+      newOption.text = book.name;
+      newOption.value = book.id;
+      comboBooks.add(newOption);
+    }
+  });
+}
+
+function createCard(){
+  let comboVisitors = document.getElementById('comboVisitors');
+  let comboBooks = document.getElementById('comboBooks');
+
+  let visitor = comboVisitors.options[comboVisitors.selectedIndex].text;
+  let idVisitor = comboVisitors.options[comboVisitors.selectedIndex].value;
+  editVisitorRating(idVisitor);
+
+  let book = comboBooks.options[comboBooks.selectedIndex].text;
+  let idBook = comboBooks.options[comboBooks.selectedIndex].value;
+  editBookRating(idBook);
+  editBookQuantity(idBook, -1); // книгу забирают
+
+  let lastId = getLastId('cards');
+  addCardToTable(lastId, visitor, book, today(), null);
+  addCardToStorage(visitor, book, idBook);
+
+  closeModalDialog('modalCard');
+}
+
+function editVisitorRating(id){
+  let visitors = JSON.parse(localStorage.getItem('visitors'));
+
+  for (let i = 0; i < visitors.length; i++){
+    if (visitors[i].id == id){
+      visitors[i].rating++;
+
+      break;
+    }
+  }
+
+  localStorage.setItem('visitors', JSON.stringify(visitors));
+}
+
+function editBookRating(id){
+  let books = JSON.parse(localStorage.getItem('books'));
+
+  for (let i = 0; i < books.length; i++){
+    if (books[i].id == id){
+      books[i].rating++;
+
+      break;
+    }
+  }
+
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function editBookQuantity(id, num){
+  let books = JSON.parse(localStorage.getItem('books'));
+
+  for (let i = 0; i < books.length; i++){
+    if (books[i].id == id){
+      if (num > 0){
+        books[i].quantity++;
+      }
+      else{
+        books[i].quantity--;
+      }
+
+      break;
+    }
+  }
+
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function addCardToTable(id, nameVisitor, nameBook, borrowDate, returnDate){
+  let newItem = document.createElement('tr');
+  let del = "del" + id;
+
+  newItem.id = "rowId" + id;
+  let childNode = returnDate == null ? `<i onclick="dateReturnBook(${newItem.id}, ${del})" id="${del}" class="fa fa-reply btn" aria-hidden="true"></i>` : returnDate;
+
+  newItem.innerHTML = `
+    <td>${id}</td>
+    <td>${nameVisitor}</td>
+    <td>${nameBook}</td>
+    <td>${borrowDate}</td>
+    <td>${childNode}</td>`;
+
+    listCards.appendChild(newItem);
+}
+
+function addCardToStorage(nameVisitor, nameBook, idBook){
+  let arrayObj = localStorage.getItem('cards');
+  let cards = [];
+
+  if (arrayObj != null){
+    cards = JSON.parse(arrayObj);
+  }
+
+  let newCard = {
+    id: getLastId('cards'),
+    visitor: nameVisitor,
+    book: nameBook,
+    idBook: idBook,
+    borrowDate: today(),
+    returnDate: null
+  };
+
+  cards.push(newCard);
+
+  localStorage.setItem('cards', JSON.stringify(cards));
+}
+
+function dateReturnBook(idRow, idDel){
+  let cards = JSON.parse(localStorage.getItem('cards'));
+
+  for (let i = 0; i < cards.length; i++){
+    if (cards[i].id == idRow.children[0].innerText){
+      cards[i].returnDate = today();
+
+      editBookQuantity(cards[i].idBook, 1); // книгу вернули
+
+      break;
+    }
+  }
+  
+  localStorage.setItem('cards', JSON.stringify(cards));
+
+  idRow.children[4].removeChild(idDel);
+  idRow.children[4].innerText = today();
+}
+
+function cardsSortBy(strSortBy){
+  let arrayObj = localStorage.getItem('cards');
+  let cards = [];
+
+  if (arrayObj != null){
+    cards = JSON.parse(arrayObj);
+  }
+
+  if (cards.length == 0){
+    let modal = document.getElementById('modal');
+    modal.innerHTML = createMessageBox('Message', 'The data array for sorting is empty');
+    modal.style.display = 'block';
+    
+    return;
+  }
+
+  switch (strSortBy){
+    case "ID":
+      cards.sort((a, b) => a.id - b.id);
+      break;
+    case "Visitor":
+      cards.sort((a, b) => a.visitor.localeCompare(b.visitor));
+      break;
+    case "Book":
+      cards.sort((a, b) => a.book.localeCompare(b.book));
+      break;
+    case "Borrow Date":
+      cards.sort((a, b) => {
+        let arrDate1 = a.borrowDate.split('.');
+        let arrDate2 = b.borrowDate.split('.');
+        let date1 = new Date(arrDate1[2], arrDate1[1] - 1, arrDate1[0]);
+        let date2 = new Date(arrDate2[2], arrDate2[1] - 1, arrDate2[0]);
+
+        if (date1 > date2) return 1;
+        else if (date1 < date2) return -1;
+        else return 0;
+      });
+      break;
+    case "Return Date":
+      cards.sort((a, b) => {
+        let strDate1 = a.returnDate;
+        let strDate2 = b.returnDate;
+
+        if (strDate1 == null && strDate2 == null){
+          strDate1 = "01." + "01." + "9999";
+          strDate2 = "01." + "01." + "9999";
+        }
+        else if (strDate1 == null){
+          strDate1 = "01." + "01." + "9999";
+        }
+        else if (strDate2 == null){
+          strDate2 = "01." + "01." + "9999";
+        }
+
+        let arrDate1 = strDate1.split('.');
+        let arrDate2 = strDate2.split('.');
+
+        let date1 = new Date(arrDate1[2], arrDate1[1] - 1, arrDate1[0]);
+        let date2 = new Date(arrDate2[2], arrDate2[1] - 1, arrDate2[0]);
+
+        
+        if (date1 > date2) return 1;
+        else if (date1 < date2) return -1;
+        else return 0;
+      });
+      break;
+  }
+
+  deleteTableRows('listCards');
+
+  cards.forEach(card => {
+    addCardToTable(card.id, card.visitor, card.book, card.borrowDate, card.returnDate);
+  });
+}
+
+function cardsSearch(searchText){
+  let arrayObj = localStorage.getItem('cards');
+  let cards = [];
+  let foundCards = [];
+
+  if (arrayObj != null){
+    cards = JSON.parse(arrayObj);
+  }
+
+  if (cards.length == 0){
+    let modal = document.getElementById('modal');
+    modal.innerHTML = createMessageBox('Message', 'The data array for searching is empty');
+    modal.style.display = 'block';
+    
+    return;
+  }
+
+  cards.forEach(card => {
+    let strBookLowerCase = card.book.toLowerCase();
+    let strVisitorLowerCase = card.visitor.toLowerCase();
+    let strReturnDate = card.returnDate;
+
+    if (strReturnDate == null){
+      strReturnDate = "01." + "01." + "9999";
+    }
+
+    if (strBookLowerCase.indexOf(searchText) != -1){
+      foundCards.push(card);
+    }
+    else if (strVisitorLowerCase.indexOf(searchText) != -1){
+      foundCards.push(card);
+    }
+    else if (card.borrowDate.indexOf(searchText) != -1){
+      foundCards.push(card);
+    }
+    else if (card.returnDate != null && card.returnDate.indexOf(searchText) != -1){
+      foundCards.push(card);
+    }
+  });
+
+  if (foundCards.length != 0){
+    deleteTableRows('listCards');
+    
+    foundCards.forEach(card => {
+      addCardToTable(card.id, card.visitor, card.book, card.borrowDate, card.returnDate);
+    });
+  }
+}
+
+function today(){
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  month = month < 10 ? '0' + month : month;
+  let day = today.getDate();
+  day = day < 10 ? '0' + day : day;
+
+  return day + '.' + month + '.' + year;
+}
+
 //=================================== GENERAL FUNCTIONS ==========================================
 
 function createMessageBox(titleMsg, textMsg){
